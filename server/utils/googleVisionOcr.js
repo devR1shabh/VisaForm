3873394d9c7a3fdf011ca3dc7be3ff7730dd6665
devR1shabh <r1shabh264237215@gmail.com>
2026-05-22 15:30:18 +0530

    support render env google vision credentials

--- server/utils/googleVisionOcr.js
+++ server/utils/googleVisionOcr.js
@@ -2,13 +2,36 @@ const fs = require("fs");
 const path = require("path");
 const vision = require("@google-cloud/vision");
 
-const defaultKeyFile = path.join(__dirname, "..", "config", "vision-key.json");
-const keyFilename = process.env.GOOGLE_APPLICATION_CREDENTIALS ||
-  (fs.existsSync(defaultKeyFile) ? defaultKeyFile : undefined);
-
-const client = new vision.ImageAnnotatorClient(
-  keyFilename ? { keyFilename } : {}
+const defaultKeyFile = path.join(
+  __dirname,
+  "..",
+  "config",
+  "vision-key.json"
 );
+
+let client;
+
+if (process.env.GOOGLE_CREDENTIALS_JSON) {
+  const credentials = JSON.parse(
+    process.env.GOOGLE_CREDENTIALS_JSON
+  );
+
+  client = new vision.ImageAnnotatorClient({
+    credentials,
+  });
+
+  console.log("[Vision] Using Render env credentials");
+} else if (fs.existsSync(defaultKeyFile)) {
+  client = new vision.ImageAnnotatorClient({
+    keyFilename: defaultKeyFile,
+  });
+
+  console.log("[Vision] Using local vision-key.json");
+} else {
+  client = new vision.ImageAnnotatorClient();
+
+  console.warn("[Vision] No credentials found");
+}
 
 async function extractTextFromImage(imageBuffer) {
   const [result] = await client.textDetection({
@@ -17,26 +40,41 @@ async function extractTextFromImage(imageBuffer) {
     },
   });
 
-  return result?.fullTextAnnotation?.text || result?.textAnnotations?.[0]?.description || "";
+  return (
+    result?.fullTextAnnotation?.text ||
+    result?.textAnnotations?.[0]?.description ||
+    ""
+  );
 }
 
 async function extractPassportOcr(images) {
   const rotatedFullImages = images.rotatedFullImages || [];
+
   const ocrTasks = [
     images.fullImage,
     images.mrzImage,
     images.lowerMrzImage,
     ...rotatedFullImages,
   ].map(extractTextFromImage);
-  const [fullText, mrzText, lowerMrzText, ...rotatedTexts] =
-    await Promise.all(ocrTasks);
+
+  const [
+    fullText,
+    mrzText,
+    lowerMrzText,
+    ...rotatedTexts,
+  ] = await Promise.all(ocrTasks);
 
   return {
     fullText,
     mrzText,
     lowerMrzText,
     rotatedTexts,
-    combinedText: [fullText, mrzText, lowerMrzText, ...rotatedTexts]
+    combinedText: [
+      fullText,
+      mrzText,
+      lowerMrzText,
+      ...rotatedTexts,
+    ]
       .filter(Boolean)
       .join("\n"),
   };
